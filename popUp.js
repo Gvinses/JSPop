43ea80b6dd7f30a8fa74f0f71f48f5b9df277de2
Gvins <timurkhen10@gmail.com>
2025-03-15 00:46:12 +0300

    childs update

--- popUp.js
+++ popUp.js
@@ -1,18 +1,46 @@
 function create_UUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r)
-        return v.toString(16);
-    });
+        const r = Math.random() * 16 | 0,
+            v = c === 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+    })
 }
 
-
 class PopUp {
+    static allPopups = new Map()
+
     constructor(content, ...events) {
         this.content = content
         this.events = events
         this.id = create_UUID()
+        this.parentId = null
+        PopUp.allPopups.set(this.id, this)
     }
+
+    createChild(content, ...events) {
+        const child = new PopUp(content, ...events)
+        child.parentId = this.id
+        return child
+    }
+
+    postToParent(data) {
+        if (!this.parentId) {
+            console.warn('No parent set for:', this.id)
+            return
+        }
+
+        const parent = PopUp.allPopups.get(this.parentId)
+        if (parent) {
+            parent.receiveData(data, this.id)
+        } else {
+            console.warn(`Parent ${this.parentId} not found for:`, this.id)
+        }
+    }
+
+    receiveData(data, childId) {
+        console.log(`[${this.id}] Received from ${childId}:`, data)
+    }
+
     applyFunctions() {
         this.events.forEach((event) => {
             let HTML_element = document.querySelector(event.origin)
@@ -20,7 +48,18 @@ class PopUp {
         })
     }
 
-    setTemplateStyles() {
+    load() {
+        this.applyFunctions()
+    }
+
+    deleteFromHTML() {
+        this.content.template.remove()
+        PopUp.allPopups.delete(this.id)
+    }
+
+
+
+    show() {
         if (this.content.display !== undefined) {
             this.content.template.style.display = String(this.content.display)
         } else {
@@ -28,27 +67,14 @@ class PopUp {
         }
     }
 
-    render() {
-        this.applyFunctions()
-        this.setTemplateStyles()
+    hide() {
+        this.content.template.style.display = 'none'
     }
-
-    show() {
-
-    }
-
-    getKey() {
-        return this.id
-    }
-
-    getDataFromChild() {}
-
-    postDataToParent() {}
-
 }
 
 let body = document.querySelector('body')
 let popup = document.querySelector('#testPopup')
+let qweqwe = document.querySelector('#testChildPopup')
 
 let testPopUp = new PopUp(
     {
@@ -60,7 +86,9 @@ let testPopUp = new PopUp(
         'origin': '#closePopupButton',
         'event': 'click',
         'func': (event) => {
-            console.log(event)
+            console.log(testPopUp)
+            testPopUp.deleteFromHTML()
+            console.log(PopUp.allPopups)
             popup.style.background = 'red'
         }
     },
@@ -68,10 +96,42 @@ let testPopUp = new PopUp(
         'origin': '#secondPopupButton',
         'event': 'click',
         'func': (event) => {
-            console.log(event)
+            let child = testPopUp.createChild(
+                {
+                    'origin': body,
+                    'template': qweqwe,
+                }
+            )
+            child.events = [
+                {
+                    'origin': '#closeChilePopupButton',
+                    'event': 'click',
+                    'func': (event) => {
+                        child.postToParent('Hello, world!', child.id)
+                        popup.style.background = 'blue'
+                    }
+                },
+                {
+                    'origin': '#secondChildPopupButton',
+                    'event': 'click',
+                    'func': (event) => {
+                        child.postToParent('Pickme meme', child.id)
+                        popup.style.background = `pink`
+                        setTimeout(() => {
+                            child.hide()
+                        }, 1500)
+                    }
+                },
+            ]
+            child.load()
+            child.show()
             popup.style.background = `green`
         }
     },
 )
 
-testPopUp.render()
+testPopUp.load()
+
+function showPopUp() {
+    testPopUp.show()
+}
